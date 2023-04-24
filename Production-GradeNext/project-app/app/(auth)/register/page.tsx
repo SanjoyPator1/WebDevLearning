25723d564797efb4049caf8598b6a8e8a090e918
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-04-24 11:06:06 +0530

    next.js project app partially completed

---
+++ Production-GradeNext/project-app/app/(auth)/register/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+import AuthForm from '@/components/Authform'
+
+// ROUTE : /register
+
+const RegisterHome = () => {
+  return (
+    <div>
+      <AuthForm mode='register' />
+    </div>
+  )
+}
+
+export default RegisterHome
