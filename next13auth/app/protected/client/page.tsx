56eb263cdf3a1be9993d6920bd96027558841f68
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-18 13:24:37 +0530

    deploy

--- next13auth/app/protected/client/page.tsx
+++ next13auth/app/protected/client/page.tsx
@@ -16,7 +16,7 @@ const ClientProtectPage = () => {
       <div className='container'>
         <h1 className='text-2xl font-bold'>
           This is a <span className='text-emerald-500'>client-side</span>{' '}
-          protected page
+          protected page.
         </h1>
         <h2 className='mt-4 font-medium'>You are logged in as:</h2>
         <p className='mt-4'>{session?.user?.name}</p>
