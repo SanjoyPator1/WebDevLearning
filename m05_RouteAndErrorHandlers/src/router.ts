460240a44bbf0368cbd3ee1ef7428045ca0cff82
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-25 12:00:04 +0530

    route and error handlers complete

--- m05_RouteAndErrorHandlers/src/router.ts
+++ m05_RouteAndErrorHandlers/src/router.ts
@@ -81,4 +81,9 @@ router.post(
 
 router.delete("/updatepoint/:id", (req, res) => {});
 
+router.use((err, req, res, next)=>{
+  console.log(err);
+  res.json({message: 'error in router handler'})
+})
+
 export default router;
--- m05_RouteAndErrorHandlers/src/server.ts
+++ m05_RouteAndErrorHandlers/src/server.ts
@@ -24,10 +24,13 @@ app.use((req,res, next)=>{
   next();
 })
 
-app.get("/", (req, res) => {
-  // sending back a json
+//this main route throws async error with next - yes we can use next in middleware
+app.get("/", (req, res, next) => {
   console.log("main route hit");
-  res.json({ message: "hello from server" });
+  // res.json({ message: "hello from server" });
+  setTimeout(()=>{
+    next(new Error('main route error'));
+  },1)
 });
 
 //protect only applies to this and not the others
@@ -36,5 +39,16 @@ app.use('/api',protect,  router)
 //this are accessible to all i.e. not protected routes
 app.use('/user', createNewUser)
 app.use('/signin', signin)
+
+// to handle async error
+app.use((err, req, res,next) => {
+  if(err.type === 'auth'){
+    res.status(401).json({message: 'unauthorized'})
+  }else if(err.type === 'input'){
+    res.status(400).json({message: 'invalid input'})
+  }else{
+    res.status(500).json({message: 'oops, thats on us'})
+  }
+})
 
 export default app;
