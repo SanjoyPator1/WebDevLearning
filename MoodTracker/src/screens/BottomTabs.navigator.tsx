349f78958db4c4d694f93a1bedc347d6b7599648
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-05-31 18:06:59 +0530

    Navbar Icons and Screen title

--- MoodTracker/src/screens/BottomTabs.navigator.tsx
+++ MoodTracker/src/screens/BottomTabs.navigator.tsx
@@ -3,8 +3,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from './Home.screen';
 import { History } from './History.screen';
 import { Analytics } from './Analytics.screen';
-import { Text } from 'react-native';
-import { HomeIcon } from '../components/Icons';
+import { AnalyticsIcon, HistoryIcon, HomeIcon } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -12,16 +12,37 @@ export const BottomTabsNavigator: React.FC = () => {
   return (
     <BottomTabs.Navigator
       screenOptions={({ route }) => ({
-        tabBarIcon: () => {
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
           if (route.name === 'Home') {
-            return <HomeIcon />;
+            return <HomeIcon color={color} size={size} />;
           }
-          return <Text>{route.name}</Text>;
+          if (route.name === 'History') {
+            return <HistoryIcon color={color} size={size} />;
+          }
+          if (route.name === 'Analytics') {
+            return <AnalyticsIcon color={color} size={size} />;
+          }
+          return null;
         },
       })}>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="History" component={History} />
-      <BottomTabs.Screen name="Analytics" component={Analytics} />
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Today's Mood" }}
+      />
+      <BottomTabs.Screen
+        name="History"
+        component={History}
+        options={{ title: 'Past Mood' }}
+      />
+      <BottomTabs.Screen
+        name="Analytics"
+        component={Analytics}
+        options={{ title: 'Analytics' }}
+      />
     </BottomTabs.Navigator>
   );
 };
