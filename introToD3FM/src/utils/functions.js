49ed1412d159709611ffbfb3f8fd5b927b89261e
SanjoyPator <sanjoy18-378@aec.ac.in>
2023-08-30 08:53:58 +0530

    monaco editor implemented init

--- introToD3FM/src/utils/functions.js
+++
@@ -1,9 +0,0 @@
-export function getTrueKeys(obj) {
-  const trueKeys = [];
-  for (const [key, value] of Object.entries(obj)) {
-    if (value === true) {
-      trueKeys.push(key);
-    }
-  }
-  return trueKeys;
-}
