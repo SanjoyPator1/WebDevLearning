e02146e4491f0d5e859b0e98d83754f40d91b5a2
sanjoypator1 <sanjoypator1@gmail.com>
2023-09-01 10:20:00 +0530

    monaco editor edit and read only done

--- editor-react/src/pages/MonacoEditor/MonacoEditorPage.tsx
+++ editor-react/src/pages/MonacoEditor/MonacoEditorPage.tsx
@@ -1,4 +1,5 @@
 import CodeEditor from "@/components/MonacoEditor/CodeEditor";
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 const MonacoEditorPage = () => {
@@ -12,6 +13,8 @@ const MonacoEditorPage = () => {
     linesCount: 1,
   });
 
+  const [isEditable, setIsEditable] = useState(true);
+
   const handleCodeEditorChange = (value: string, linesCount: number) => {
     setCodeEditorData({ codeValue: value, linesCount: linesCount });
   };
@@ -19,13 +22,19 @@ const MonacoEditorPage = () => {
   return (
     <div className="flex  flex-col gap-md">
       <h2>MonacoEditorPage</h2>
+      <Button onClick={() => setIsEditable((prev) => !prev)} className="w-fit">
+        {isEditable ? "View Only" : "Edit"}
+      </Button>
       <div className="h-[60dvh] border-3">
         <CodeEditor
           codeDataProp={CodeEditorData}
           onChange={handleCodeEditorChange}
+          isEditable={isEditable}
         />
         {CodeEditorData.linesCount > 15 && (
-          <p className="text-destructive">Number of lines of code must be less than or equal to 15</p>
+          <p className="text-destructive">
+            Number of lines of code must be less than or equal to 15
+          </p>
         )}
       </div>
     </div>
